9c1415fd01acbb85d59de72f23e71e7ab982eacd
Yuriy <ternxstyle@gmail.com>
2020-01-17 05:30:07 +0200

    global change on the resume side

--- js/popup_projects.js
+++ js/popup_projects.js
@@ -1,6 +1,6 @@
-let modal = document.getElementById("myModal");
-let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
     modal.style.display = "block";
